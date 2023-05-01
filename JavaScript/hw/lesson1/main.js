64a72326dadf445e2ad6943f6164f82b70ecca28
User <dfds@jbj.com>
2023-05-01 21:32:04 +0300

    first commit

---
+++ JavaScript/hw/lesson1/main.js
@@ -0,0 +1,48 @@
+// part1
+let str = 'hello';
+console.log(str)
+let str1 = 'owu';
+console.log(str1)
+let str2 = 'com';
+console.log(str2)
+let str3 = 'ua';
+console.log(str3)
+let num = 1;
+console.log(num)
+let num1 = 10;
+console.log(num1)
+let num2 = -999;
+console.log(num2)
+let num3 = 123;
+console.log(num3)
+let num4 = 3.14;
+console.log(num4)
+let num5 = 2.7;
+console.log(num5)
+let num6 = 16;
+console.log(num6)
+let bool = true;
+console.log(bool)
+let bool1 = false;
+console.log(bool1)
+
+//part2
+let firstName = 'Andrew';
+let middleName = 'Igorevich';
+let lastName = 'Kutovy';
+let person = `${lastName} ${firstName} ${middleName}`;
+
+//part3
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
+
+//part4
+let name = prompt('Enter your name: ')
+let secondName = prompt('Enter your second name: ')
+let age = prompt('Enter your age: ')
+console.log(`Your name is: ${name}, your second name is: ${secondName}, you'r ${age} years old.`)
+
