88e34e268bc13588dfee3965a5a4532460745b6c
User <dfds@jbj.com>
2023-05-04 13:18:25 +0300

    hw 2 added + in prompt for age

--- JavaScript/hw/lesson1/main.js
+++ JavaScript/hw/lesson1/main.js
@@ -1,30 +1,30 @@
 // part1
 let str = 'hello';
-console.log(str)
+console.log(str);
 let str1 = 'owu';
-console.log(str1)
+console.log(str1);
 let str2 = 'com';
-console.log(str2)
+console.log(str2);
 let str3 = 'ua';
-console.log(str3)
+console.log(str3);
 let num = 1;
-console.log(num)
+console.log(num);
 let num1 = 10;
-console.log(num1)
+console.log(num1);
 let num2 = -999;
-console.log(num2)
+console.log(num2);
 let num3 = 123;
-console.log(num3)
+console.log(num3);
 let num4 = 3.14;
-console.log(num4)
+console.log(num4);
 let num5 = 2.7;
-console.log(num5)
+console.log(num5);
 let num6 = 16;
-console.log(num6)
+console.log(num6);
 let bool = true;
-console.log(bool)
+console.log(bool);
 let bool1 = false;
-console.log(bool1)
+console.log(bool1);
 
 //part2
 let firstName = 'Andrew';
@@ -43,6 +43,6 @@ console.log(typeof c)
 //part4
 let name = prompt('Enter your name: ')
 let secondName = prompt('Enter your second name: ')
-let age = prompt('Enter your age: ')
+let age = +prompt('Enter your age: ')
 console.log(`Your name is: ${name}, your second name is: ${secondName}, you'r ${age} years old.`)
 
